2d3e5b93780d6dae4654b50225aa5e902e5b226e
Anshul015 <124901063+Anshul015@users.noreply.github.com>
2026-07-25 18:46:19 +0530

    Add back-to-top button functionality

--- script.js
+++ script.js
@@ -61,3 +61,19 @@ lightbox.addEventListener("click", (e) => {
         lightbox.style.display = "none";
     }
 });
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 300){
+        backToTop.style.display = "block";
+    }else{
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
