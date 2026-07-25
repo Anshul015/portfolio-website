dbd194fd2e6ad6319c983cda462a792fd0da0bf1
Anshul015 <124901063+Anshul015@users.noreply.github.com>
2026-07-25 14:20:23 +0530

    Add Intersection Observer for fade-in sections

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ topBtn.onclick = function () {
     behavior: "smooth"
   });
 };
+const sections = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+sections.forEach(section=>{
+observer.observe(section);
+});
