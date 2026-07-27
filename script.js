c31308d371ef35ba25b3fb106646bf2d8709c0ca
Anshul015 <124901063+Anshul015@users.noreply.github.com>
2026-07-27 12:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,3 +104,16 @@ window.addEventListener("load", () => {
     }, 600);
 
 });
+window.addEventListener("scroll", () => {
+
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progressBar").style.width =
+        progress + "%";
+
+});
